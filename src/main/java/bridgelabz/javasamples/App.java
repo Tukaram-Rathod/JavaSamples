e34857ed6f6bc6b0714674a2321e34a098e7d9f0
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-21 13:26:59 +0530

    ADD. added java samples files to local

---
+++ src/main/java/bridgelabz/javasamples/App.java
@@ -0,0 +1,24 @@
+package bridgelabz.javasamples;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG= LogManager.getLogger(App.class);
+	public static void main( String[] args )
+    {
+		String message = "Hello World! ";
+		LOG.debug(message + " Will be Printed On Debug ");
+		LOG.info(message + " Will be Printed On Info ");
+		LOG.warn(message + " Will be Printed On Warn ");
+		LOG.error(message + " Will be Printed On Error ");
+		LOG.fatal(message + " Will be Printed On Fatal ");
+		LOG.info("Appending string :{}.",message);
+        System.out.println( message );
+    }
+}
